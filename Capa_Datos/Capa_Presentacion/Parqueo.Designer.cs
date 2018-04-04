38e8cfb2a60adadfb9fb962abaaed70e108e460d
punts hola mundo <anthonnyc10@gmail.com>
2018-04-04 17:58:25 -0600

    Se agrego la libreria Speech y se realizo un poco de interfaz.

--- Capa_Datos/Capa_Presentacion/Parqueo.Designer.cs
+++ Capa_Datos/Capa_Presentacion/Parqueo.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Parqueo));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnParqueo = new System.Windows.Forms.Button();
+            this.Carro = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Carro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(316, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Parqueo";
+            // 
+            // btnParqueo
+            // 
+            this.btnParqueo.Location = new System.Drawing.Point(428, 13);
+            this.btnParqueo.Name = "btnParqueo";
+            this.btnParqueo.Size = new System.Drawing.Size(75, 23);
+            this.btnParqueo.TabIndex = 1;
+            this.btnParqueo.Text = "Parquear";
+            this.btnParqueo.UseVisualStyleBackColor = true;
+            this.btnParqueo.Click += new System.EventHandler(this.btnParqueo_Click);
+            // 
+            // Carro
+            // 
+            this.Carro.Image = ((System.Drawing.Image)(resources.GetObject("Carro.Image")));
+            this.Carro.Location = new System.Drawing.Point(364, 298);
+            this.Carro.Name = "Carro";
+            this.Carro.Size = new System.Drawing.Size(126, 86);
+            this.Carro.TabIndex = 2;
+            this.Carro.TabStop = false;
+            // 
+            // Parqueo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(896, 533);
+            this.Controls.Add(this.Carro);
+            this.Controls.Add(this.btnParqueo);
+            this.Controls.Add(this.label1);
+            this.Name = "Parqueo";
             this.Text = "Parqueo";
+            this.Load += new System.EventHandler(this.Parqueo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Carro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnParqueo;
+        private System.Windows.Forms.PictureBox Carro;
     }
 }
--- Capa_Datos/Capa_Presentacion/Parqueo.cs
+++ Capa_Datos/Capa_Presentacion/Parqueo.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Recognition;
 
 namespace Capa_Presentacion
 {
     public partial class Parqueo : Form
     {
+
+        private SpeechRecognitionEngine escucha = new SpeechRecognitionEngine();
         public Parqueo()
         {
             InitializeComponent();
         }
+
+        private void Parqueo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnParqueo_Click(object sender, EventArgs e)
+        {
+
+            label1.Visible = false;
+            try
+            {
+                escucha.SetInputToDefaultAudioDevice();
+                escucha.LoadGrammar(new DictationGrammar());
+                escucha.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Lector);
+                escucha.RecognizeAsync(RecognizeMode.Multiple);
+
+
+
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("No se puede abrir el boton tantas veces");
+            }
+        }
+
+
+        public void Lector(object sender, SpeechRecognizedEventArgs e)
+        {
+            foreach (RecognizedWordUnit palabra in e.Result.Words)
+            {
+                label1.Text = palabra.Text;
+
+                if (palabra.Text.Equals("Izquierda"))
+                {
+                    Carro.Location = new Point(Carro.Location.X - 20, Carro.Location.Y);
+                }
+
+            }
+        }
+
     }
+
 }
+
+
