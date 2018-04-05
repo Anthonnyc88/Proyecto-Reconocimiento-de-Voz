c507eea68ee10f9d1179b23240031af001a68692
ROGER ALBERTO OPORTA PEREZ <raoportap@est.utn.ac.cr>
2018-04-05 09:09:49 -0600

    creo archivo xml carros

---
+++ Capa_Datos/Capa_Datos/XML_Carros.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Capa_Datos
+{
+    public class XML_Carros
+    {
+        string rutaXml = "Automoviles.xml";
+        XmlDocument doc = new XmlDocument();
+
+        public void _crearXml(string ruta, string nodoRaiz)
+        {
+            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlNode root = doc.DocumentElement;
+            doc.InsertBefore(xmlDeclaration, root);
+
+            XmlNode element1 = doc.CreateElement(nodoRaiz);
+            doc.AppendChild(element1);
+            doc.Save(ruta);
+        }
+
+        public void _Añadir_Automovil(string placa, string f_p)
+        {
+            doc.Load(rutaXml);
+
+            XmlNode auto = _Crear_Automovil(placa, f_p);
+
+            XmlNode nodoRaiz = doc.DocumentElement;
+
+            nodoRaiz.InsertAfter(auto, nodoRaiz.LastChild);
+
+            doc.Save(rutaXml);
+        }
+
+        private XmlNode _Crear_Automovil(string placa, string fecha_parqueo)
+        {
+            XmlNode auto = doc.CreateElement("automovil");
+
+            XmlElement xplaca = doc.CreateElement("placa");
+            xplaca.InnerText = placa;
+            auto.AppendChild(xplaca);
+
+            XmlElement xfecha_parqueo = doc.CreateElement("fecha_parqueo");
+            xfecha_parqueo.InnerText = fecha_parqueo;
+            auto.AppendChild(xfecha_parqueo);
+            
+            return auto;
+        }
+    }
+}
